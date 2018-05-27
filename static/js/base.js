802c42192606a696543410e0f4961fc3bca2b55b
jonan <jonan@DESKTOP-E1P2GC2>
2018-05-27 23:44:07 +0200

    dbs and looks

--- static/js/base.js
+++ static/js/base.js
@@ -1,10 +1,27 @@
-
+if (sessionStorage.getItem("logginView") == "active") {
+	initToggleMenus(document.getElementById("loggin-button"),
+			"loggin-container");
+} else {
+	sessionStorage.setItem("logginView", "inactive")
+}
 /**
  * 
  * @param menuElement
  * @returns
  */
 function toggleMenus(menuElement, id) {
+	menuElement.classList.toggle("toggle");
+	var element = document.getElementById(id);
+	element.classList.toggle("toggle");
+	if (id == "loggin-container") {
+		if (sessionStorage.getItem("logginView") == "active") {
+			sessionStorage.setItem("logginView", "inactive")
+		} else {
+			sessionStorage.setItem("logginView", "active")
+		}
+	}
+}
+function initToggleMenus(menuElement, id) {
 	menuElement.classList.toggle("toggle");
 	var element = document.getElementById(id);
 	element.classList.toggle("toggle");
